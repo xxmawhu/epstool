5556fd6fbd9e112702d227cadec30401d064c974
xxmawhu <xxmawhu@163.com>
2020-02-21 21:54:44 +0800

    update

--- tool/cut.py
+++ tool/cut.py
@@ -1,39 +1,57 @@
 import subprocess
 import sys
+import glob
+
+
 def getBound(fileName):
-    commands=['gs', '-dNOPAUSE', '-dBATCH', '-q', '-sDEVICE=bbox']
+    commands = ['gs', '-dNOPAUSE', '-dBATCH', '-q', '-sDEVICE=bbox']
     commands.append(fileName)
     pr = subprocess.Popen(commands, stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE)
+                          stderr=subprocess.PIPE)
     out, err = pr.communicate()
     output = err
     try:
         return output.decode()
-    except Expection:
-        print("decode fail")
+    except Exception:
+        print("decode fail {}".format(Exception))
         return output
 
+
 def changeBound(fileName, newBound=''):
-    s=''
+    # print("newBound {}".format(newBound))
+    s = ''
     for line in open(fileName, 'r').readlines():
         if "HiResBoundingBox" in line:
             continue
-        if "BoundingBox" in line and not "HiResBoundingBox" in line:
-            s += newBound
+        if "BoundingBox" in line and "HiResBoundingBox" not in line:
+            try:
+                s += newBound
+            except UnicodeDecodeError as e:
+                s += str(newBound)
+            except Exception as e:
+                print(e)
             continue
         s += line
     return s
 
+
 def main(fileName):
+    print("Process {}".format(fileName))
     bound = getBound(fileName)
     s = changeBound(fileName, bound)
     f = open(fileName, 'w')
     f.write(s)
     f.close()
 
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Please provide the eps file:")
         exit(1)
-    fileName = sys.argv[1]
-    main(fileName)
+    for fileName in sys.argv[1:]:
+        for f in glob.glob(fileName):
+            try:
+                main(f)
+            except Exception as e:
+                print(e)
+                continue
